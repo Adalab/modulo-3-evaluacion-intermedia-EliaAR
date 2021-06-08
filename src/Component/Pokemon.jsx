6ba01c4cf34adf6343bc0c944c4d3da1a2a58a3a
EliaAR <alonsoramos.elia@gmail.com>
2021-06-08 10:56:59 +0200

    add new build

--- src/Component/Pokemon.jsx
+++ src/Component/Pokemon.jsx
@@ -3,9 +3,17 @@ import "../stylesheet/Pokemos.scss";
 function Pokemon(props) {
   return (
     <li className="pokemos">
-      <img src={props.item.url} alt={props.item.name} />
+      <img
+        className="pokemos__img"
+        src={props.item.url}
+        alt={props.item.name}
+      />
       <h3 className="pokemos__name">{props.item.name}</h3>
-      <p className="pokemos__type">{props.item.types}</p>
+      <p className="pokemos__types">
+        {props.item.types.map((type) => (
+          <li className="pokemos__type">{type}</li>
+        ))}
+      </p>
     </li>
   );
 }
